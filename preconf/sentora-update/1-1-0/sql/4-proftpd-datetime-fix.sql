4b57d4dac3c6a0448eac9b1ccfc4a6145121f404
Duke City Solutions, LLC <36783248+Dukecitysolutions@users.noreply.github.com>
2020-08-05 20:36:35 -0600

    Update 4-proftpd-datetime-fix.sql

--- preconf/sentora-update/1-1-0/sql/4-proftpd-datetime-fix.sql
+++ preconf/sentora-update/1-1-0/sql/4-proftpd-datetime-fix.sql
@@ -11,9 +11,10 @@ USE `sentora_proftpd`;
 
 /* Update/fix ALL Proftpd ftpuser table with users w/modified 0000-00-00 00:00:00 value TO NULL for NO_ZREO_DATE & NO_ZERO_IN_DATE issues*/
 
-UPDATE ftpuser set modified=NULL WHERE modified='0000-00-00 00:00:00';
-
 ALTER TABLE ftpuser MODIFY COLUMN accessed datetime DEFAULT NULL;
 ALTER TABLE ftpuser MODIFY COLUMN modified datetime DEFAULT NULL;
+
+UPDATE ftpuser set modified=NULL WHERE modified='0000-00-00 00:00:00';
+
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
